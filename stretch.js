103a3e901c26cfcdc2ca8d9cffdcfcca44ee585d
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2020-11-09 12:50:51 -0800

    finished MVP and stretch

--- stretch.js
+++ stretch.js
@@ -4,4 +4,19 @@
   console.log("a defined? " + (typeof a !== 'undefined'));
   console.log("b defined? " + (typeof b !== 'undefined'));
 
+//   var addSix = createBase(6);
+//   addSix(10); // returns 16
+//   addSix(21); // returns 27
+
+  function createBase(baseNumber) {
+    return function(N) {
+      // we are referencing baseNumber here even though it was declared
+      // outside of this function. Closures allow us to do this in JavaScript
+      return baseNumber + N;
+    }
+  }
   
+  var addSix = createBase(6);
+  console.log(addSix)
+  console.log(addSix(10));
+  console.log(addSix(21));
